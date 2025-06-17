ce507772117439ec55b553002d6f7225440796e6
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-06-17 12:53:22 +0530

    learn about dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(9 + 1)
+
+let age = 24;
+let name = "VIvek";
+let isLoggedIn = true;
+let userId; // undefined
+
+// dataTypes
+
+// numbers => 2 to power 53
+// bigint => for big numbers like trading, fb, reddit etc
+// string => "" / ''
+// boolean => true / false | han or naa
+// null => empty value
+// undefined => variable is declared but not assigned just like userId
+// symbol => uniqueness
+
+// objects
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
