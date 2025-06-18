fb751b9f0a4eac15f011432c9c45d204a430aaa9
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-06-18 20:30:37 +0530

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "28SOS"
+// console.log(typeof score); 
+// console.log(typeof (score));
+
+// let age = 24;
+// console.log(typeof (age));
+
+// let valueInString = String(score); //  using String() keyword to convert number to string
+// console.log(typeof valueInFloat);
+
+// let valueInnumber = Number(score); //number() keyword convert krta hai kisi bhi data type ko number me
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// jab koi string ko convert krte hai to NaN milta hai
+// jab koi boolean ko convert krte hai to 0/1 milta hai
+// jab Undefined ko convert krte hai to NaN milta hai
+//null ko convert krte hai to 0 milta hai
+
+let isLoggedIn = 1;
+let isLoggedInBoolean = Boolean(isLoggedIn); 
+
+console.log(isLoggedInBoolean);
+
+// 1 => true | 0=> false
+// "" => false
+// "Vivek" => true
