d22b8177c1dd542eb5091da1d2ceeb5788b7d31a
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-06-20 13:52:00 +0530

    modified

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -4,32 +4,33 @@
 
 // There are 7 types of primitive data types : String, Numbeer, Boolean, Null, Undefined, Symbol, BigInt
 
-// const isLoggedIn = false;
-// const outsideTemp = null;
-// let marks = 74;
-// let userName = "Vivek";
-// let email = undefined;
+const isLoggedIn = false;
+const outsideTemp = null;
+let marks = 74;
+let userName = "Vivek";
+let email = undefined;
 
-// const Id = Symbol("123");
-// // const anotherId = Symbol("123");
+const Id = Symbol("123");
+const anotherId = Symbol("123");
 
-// let storedValue = Id;
-// console.log(storedValue === Id); // but mai storedValue ko Id ke barabar kar sakta hu, kyuki storedValue me Id ki value hai
+let storedValue = Id;
+console.log(storedValue === Id); // but mai storedValue ko Id ke barabar kar sakta hu, kyuki storedValue me Id ki value hai
 
 // console.log(Id === anotherId); // false, because symbols unique hote hai, just like classes React me
 
 //Reference (Non Primitive) data types in JavaScript
+// array, object, function
 
-// let animeCharacter = ["Luffy", "Zoro", "Nami"]; // Array
+let animeCharacter = ["Luffy", "Zoro", "Nami"]; // Array
 
-// let LuffysDetails = {
-//     fullname: "Monkey D. Luffy",
-//     age: 19,
-//     work: "Pirate",
-//     currentPosition: "Captain",
-//     wanted: "1.5 Billion Berries",
-//     isAlive: true,
-// }; 
+let LuffysDetails = {
+    fullname: "Monkey D. Luffy",
+    age: 19,
+    work: "Pirate",
+    currentPosition: "Captain",
+    wanted: "1.5 Billion Berries",
+    isAlive: true,
+}; //object
 
 // console.log(LuffysDetails); // Object
 
