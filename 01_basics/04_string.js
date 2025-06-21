3ebce3f67121cf2b0c2aa7debe332c280a447b62
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-06-21 17:44:53 +0530

    strings

---
+++ 01_basics/04_string.js
@@ -0,0 +1,8 @@
+let userName = "Vivek Kumar";
+const userAccountBalance = 100;
+let friend = "Siddeshwar";
+
+// console.log(userName + " has " + userAccountBalance + " in his account.");
+
+// Template literals `${}`
+console.log(`${userName} has ${userAccountBalance} rupees in his account and he want to share with ${friend} `);
