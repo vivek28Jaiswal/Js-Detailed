fffbcde67173060ace46aeb40764e32c24eb44ce
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-06-22 12:26:23 +0530

    Math

--- 01_basics/05_number_and_math.js
+++ 01_basics/05_number_and_math.js
@@ -9,8 +9,24 @@
 
 braceletPrice = 440.989898777;
 
-console.log(braceletPrice.toString().length); 
-console.log(braceletPrice.toFixed(2));
+// console.log(braceletPrice.toString().length); 
+// console.log(braceletPrice.toFixed(2));
+
+// console.log(braceletPrice.toPrecision(4));
+
+const hundred = 10000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ++++++++++++++++ Maths ++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-10)); // Absolute value, returns positive value always
+console.log(Math.round(4.2)); // Rounds to nearest integer means roundoff kr deta hai value ko
+console.log(Math.ceil(4.1)); // Rounds up to the next integer
+console.log(Math.floor(4.9)); // Rounds down to the previous integer
+
+
+
 
 
 
