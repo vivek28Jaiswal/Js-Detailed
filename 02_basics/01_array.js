a6e1031eb12a64a40c098bddc476e1d61dca7097
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-06-25 12:34:45 +0530

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+const arr = [10, 20, 30, "Vivek", 50, false];
+// console.log(arr);
+// console.log(typeof arr[5]);
+
+// let copyArr = new Array(1, 2, 3, "Snape", "Lily", true);  //new ek instance banata hai asaan sabdo me bole to copy and array ka type of object hai
+// console.log(copyArr[3]);
+
+// array methods
+arr.push(101,102);  // array ke last me value add kr deta hai jo user provide krta hai it can be numbers,string,boolean vlaue
+arr.pop()           // pop() method array ke last vlaue ko remove kr deta hai 
+// arr.unshift("Mr.")  // unshift("added") method array ke first me given value add kr deta hai
+// arr.shift()      // shift() method first value hata deta hai
+console.log(arr);
