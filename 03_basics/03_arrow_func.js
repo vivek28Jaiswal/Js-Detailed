c408efe09741d5c3c4542e69f298fa211e569506
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-07-05 22:19:17 +0530

    arrow func

---
+++ 03_basics/03_arrow_func.js
@@ -0,0 +1,24 @@
+let user = {
+    username: "Vivek",
+    enrollFee : 999,
+    
+    welcomeMessage(){
+        console.log((`Hey, ${this.username} Welcome`));
+        console.log(this);
+        
+    }
+}
+
+// user.welcomeMessage();
+// user.username = "Mrunal thakur"
+// user.welcomeMessage();
+// console.log(this);
+
+// function color (){
+//     favColor = "Black"
+//     console.log(this.favColor);
+    
+// }
+
+// color()
+
