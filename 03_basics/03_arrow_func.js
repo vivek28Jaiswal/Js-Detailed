b3ad16fcea1e34608342fecd87e3d902a2bc2694
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-07-06 12:35:55 +0530

    arrow

--- 03_basics/03_arrow_func.js
+++ 03_basics/03_arrow_func.js
@@ -22,3 +22,34 @@ let user = {
 
 // color()
 
+
+function color (){
+    let username = "PostPie"
+    console.log(this.username);
+    
+}
+
+// color()
+
+//ES6 arrow func aya
+//note func me this undefiend show krta hai
+//note if func ke ander curly braces hai to return likhna hoga
+//note if func implicit return me likha gya hai to curly braces hata dena hai same isme bhi no need  to write return
+//note and parentheses () me likha to return use krna nhi padega  
+
+let closeFriend = ()=>{
+    let friendname = "Sam";
+    console.log(this.friendname);
+}
+
+// closeFriend()
+
+// let addTwoNum = (num1, num2) => {
+//     return num1 + num2;
+// }
+// let addTwoNum = (num1, num2) => num1 + num2;
+// let addTwoNum = (num1, num2) => (num1 + num2)
+let addTwoNum = (num1, num2) => ({name: "UB", age: 24})
+
+
+console.log(addTwoNum());
