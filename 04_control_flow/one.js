e9c1b93663a2480ba54cd4ea73c467d3eb4e9f03
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-07-09 12:32:14 +0530

    conditions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,48 @@
+let temperature = 51
+
+// if(temperature <= 50){
+//     console.log('temperature is less than 50');
+// } else {
+//     console.log('temperature is greater than 50');
+// }
+
+//Comparison Operators
+// <, >, =, ==, ===, <=, >=, !=, !== 
+
+// let score = 300;
+// if(score < 200) {
+//     let power = "Bunge gum";
+//     console.log(`your power is ${power}`);
+// } else {
+//     let powerTwo = "Shadow Summon";
+//     console.log(`your power is ${powerTwo}`);
+    
+// }
+
+// do not write this type of code
+// let score = 600;
+// if (score > 500) console.log("Clear level 1"), console.log("Got beginner title");
+
+// let wantedBarries = 895
+// if(wantedBarries < 250 ){
+//     console.log("Criminal");
+// } else if(wantedBarries < 500){
+//     console.log("Level Secound Criminal");
+// } else if (wantedBarries < 750){
+//     console.log("Level Third Criminal");
+// } else if (wantedBarries < 1200){
+//     console.log("Most Wanted Criminal");
+// }
+
+let username = "vivek"
+let userVerfied = true
+let paymentStatus = true
+let eligibilityCheck = false
+
+// && (AND) dono side ki value or condition true hona chahiye
+if (userVerfied && paymentStatus && eligibilityCheck){
+    console.log(`${username} can access course`);
+} else {
+    console.log(`${username} cannot access this course`);
+    
+}
