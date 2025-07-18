23937d39c9ea2dcac70744fb6806dadf9a8713e9
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-07-18 12:30:13 +0530

    loops

--- 05_iterations/basicloop.js
+++ 05_iterations/basicloop.js
@@ -1,5 +1,4 @@
 //for loop
-
 for (let i = 0; i <=10; i++) {
     let number = i;
     if(number == 5){
@@ -38,7 +37,50 @@ for( let i = 0; i <= 50; i++){
 }
 // console.log(sum);
 
-// let number = 1;
+// Store all numbers between 1 to 100 that are divisible by 5 in a variable
+
+// let number = [];
 // for ( let i = 1; i <= 100; i++) {
-//     i % 5 === 0
+//     if (i % 5 === 0) {
+//         number.push(i); //arr ke end me value add kr deta hai push() method
+//     }
 // }
+// console.log(number)
+
+// let customerDetails = [
+//   { username: "RiyaKapoor" },
+//   { username: "YashGupta" },
+//   { username: "NehaJoshi" },
+//   { username: "KabirBansal" },
+//   { username: "TanyaMishra" },
+//   { username: "AdityaRana" },
+//   { username: "PoojaAgarwal" },
+//   { username: "VivaanShukla" },
+//   { username: "SnehaSaxena" },
+//   { username: "RahulSinha" }
+// ]
+
+// customerDetails.forEach(user => {
+//     console.log(`hey ${user.username} how are you?`);  
+// });
+
+//break and continue
+
+for(let i = 1; i <= 20; i++) {
+    if( i == 5){
+        // console.log(`favourite number detected`);
+        break;
+    }
+    // console.log(`value of i is ${i}`);
+    
+}
+
+//ek bar mistake ko ignore kiya jata hai, matlab ek bari condition ko skip kiya jata hai wo print nhi hota and break nhi krta chalte jata hai condition
+for(let i = 1; i <= 20; i++) {
+    if( i == 5){
+        console.log(`favourite number detected`);
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+    
+}
