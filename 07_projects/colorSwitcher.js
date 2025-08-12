a81aedb86cc2f44c1a8de3ba1ff25d941755ad3a
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-08-12 13:23:15 +0530

    project 1

---
+++ 07_projects/colorSwitcher.js
@@ -0,0 +1,24 @@
+let buttons = document.querySelectorAll('.button');
+let body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  // console.log(button.id)
+  button.addEventListener('click', function (e) {
+    console.log(e.target);
+    if (e.target.id === 'grey'){
+      body.style.backgroundColor = e.target.id
+    }
+    if (e.target.id === 'white'){
+      body.style.backgroundColor = e.target.id
+    }
+    if (e.target.id === 'blue'){
+      body.style.backgroundColor = e.target.id
+    }
+    if (e.target.id === 'yellow'){
+      body.style.backgroundColor = e.target.id
+    }
+  });
+});
+
+
+// switch method
