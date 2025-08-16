65ffb34ff6148110a0b0ccdf75de2bd6a278b316
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-08-16 15:54:29 +0530

    projects

--- 07_projects/Bmi.js
+++ 07_projects/Bmi.js
@@ -12,6 +12,14 @@ form.addEventListener('submit', function(e){
     results.innerHTML = `Enter valid ${weight}`
   }else {
     let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    let BmiWarning = document.querySelector('#BmiWarning');
     results.innerHTML = `Bmi is ${bmi}`
+    if (bmi < 18.6){
+      BmiWarning.innerHTML = `You are Under Weight`
+    } else if (bmi >= 18.6 && bmi <= 24.9){
+      BmiWarning.innerHTML = `Your BMI is normal`
+    } else {
+      BmiWarning.innerHTML = `You are overweight`
+    }
   }
 })
