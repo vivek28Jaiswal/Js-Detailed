1106146cf56356d0844359c1bf13d77e89a5e869
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-08-18 13:45:00 +0530

    guess number frontend done

---
+++ 07_projects/guessTheNum.js
@@ -0,0 +1,17 @@
+// let userInput = document.querySelector('#guessField');
+// let submit = document.querySelector('#subt')
+// let prevGuess = document.querySelector('.guesses')
+// let guessRemaining = document.querySelector('.lastResult')
+
+// let prevguess = [];
+// let numGuess = 1;
+// let playGame = true;
+
+// if(playGame){
+//   submit.addEventListener('click', function(elem){
+//     elem.preventDefault()
+//     let userValue = parseInt(userInput.value);
+//     console.log(userValue)
+//   })
+// }
+//pending completion
