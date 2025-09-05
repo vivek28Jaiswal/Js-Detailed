3242fc6aa47fe3504fa1918b3531406fee223f37
ᴠɪᴠᴇᴋ <vivekfr28@gmail.com>
2025-09-05 12:44:35 +0530

    learn apis, promises, fetch, then,catch,finally, and  try-catch

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -36,28 +36,77 @@
 //     console.log(details);
 // })
 
-const promiseFour = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let error = false;
+// const promiseFour = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     let error = false;
+//     if (!error) {
+//       resolve({ username: "vivek", age: 24, mail: "vivek@example.com" });
+//     } else {
+//       console.log("ERROR: somthing went wrong");
+//     }
+//   }, 1000);
+// });
+
+// promiseFour
+//   .then((dets) => {
+//     console.log(dets);
+//     return dets.username;
+//   })
+//   .then((user) => {
+//     console.log(user); //uper wale then se dets.username ka parameter accept kiya current then me.
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   .finally(() =>
+//     console.log("ye chalega chahe promise resolved ho ya rejected")
+//   );
+
+let promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = true;
     if (!error) {
-      resolve({ username: "vivek", age: 24, mail: "vivek@example.com" });
+      resolve({
+        brand: "apple",
+        price: "76,000RS",
+        model: "macbook m1",
+        discount: "applied",
+      });
     } else {
-      console.log("ERROR: somthing went wrong");
+      reject("somthing went wrong check your apple pass");
     }
   }, 1000);
 });
 
-promiseFour
-  .then((dets) => {
-    console.log(dets);
-    return dets.username;
-  })
-  .then((user) => {
-    console.log(user); //uper wale then se dets.username ka parameter accept kiya current then me.
-  })
-  .catch((error) => {
+async function ConsumePromiseFive() {
+  try {
+    let response = await promiseFive;
+    console.log(response);
+  } catch (error) {
     console.log(error);
-  })
-  .finally(() =>
-    console.log("ye chalega chahe promise resolved ho ya rejected")
-  );
+    
+  }
+}
+// second way to get resolve and reject using async await
+ConsumePromiseFive();
+
+async function getAllUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = response.json()
+  console.log(data);
+}
+
+// getAllUsers()
+
+//fetch using then, catch, finally
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(response => {
+  let data = response.json()
+  return data;
+})
+.then((data) =>{
+  data.forEach(user => {
+    console.log(user.name)
+  });
+})
+.catch((error) => console.log(error));                   
